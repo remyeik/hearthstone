0eee4a0f553988ac00d85da70ce8d8887db64f8a
remyeik <37485988+remyeik@users.noreply.github.com>
2025-01-14 14:13:51 +0100

    GH Pages changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (!rootElement) {
-    throw new Error('Failed to find the root element');
+    throw new Error("Failed to find the root element");
 }
 
 ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>
+    </React.StrictMode>,
 );
